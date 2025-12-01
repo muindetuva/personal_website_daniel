ab815064aa1e634610967f7dcd7100073b644177
Alfred Tuva <muindetuva@gmail.com>
2025-12-01 17:27:14 +0300

    Complete v1 of chatbot

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,53 @@
-const menu = document.querySelector('.hamburger-menu');
+const menu = document.querySelector(".hamburger-menu");
+const navLinks = document.querySelector(".nav-links");
+const closeButton = document.querySelector(".close-btn");
+const chatboxTrigger = document.querySelector("#chatbox-trigger");
+const chatboxWindow = document.querySelector(".chatbox-window");
+const chatbotCloseBtn = document.querySelector("#close-chat");
+const chatForm = document.querySelector(".chat-form");
+const chatInput = document.querySelector("#chat-input");
+const chatMessages = document.querySelector(".chat-messages");
 
-const navLinks = document.querySelector('.nav-links');
+menu.addEventListener("click", () => {
+  navLinks.style.display = "block";
+});
 
-const closeButton = document.querySelector('.close-btn');
+closeButton.addEventListener("click", () => {
+  navLinks.style.display = "none";
+});
 
-const chatboxTrigger = document.querySelector('#chatbox-trigger');
+chatboxTrigger.addEventListener("click", (e) => {
+  chatboxWindow.style.display = "flex";
+});
 
-const chatboxWindow = document.querySelector('.chatbox-window');
+chatbotCloseBtn.addEventListener("click", () => {
+  chatboxWindow.style.display = "none";
+});
 
-const chatbotCloseBtn = document.querySelector('#close-chat');
+chatForm.addEventListener("submit", (e) => {
+  // Prevent page reload
+  e.preventDefault();
 
-menu.addEventListener('click', () => {
-    navLinks.style.display = "block";
-})
+  const userText = chatInput.value;
 
-closeButton.addEventListener('click', () => {
-    navLinks.style.display = "none";
-})
+  // Displaying the user message
+  const userMessage = document.createElement("div");
+  userMessage.classList.add("msg");
+  userMessage.classList.add("user");
+  userMessage.innerText = userText;
+  chatMessages.appendChild(userMessage);
 
-chatboxTrigger.addEventListener('click', () => {
-    chatboxWindow.style.display = "flex";
-})
+  // Clear the input
+  chatInput.value = "";
 
-chatbotCloseBtn.addEventListener('click', () => {
-    chatboxWindow.style.display = "none";
-})
+  // Display bot response
+  const botText = "Hi,I'm still dumb, check back in a few days 🙃";
+  const botMessage = document.createElement("div");
+  botMessage.classList.add("msg");
+  botMessage.classList.add("bot");
+  botMessage.innerText = botText;
+
+  setTimeout(() => {
+    chatMessages.appendChild(botMessage);
+  }, 2000);
+});
